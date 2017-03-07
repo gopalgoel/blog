f6910722d12618dbb0f1100a87d8fd5da34229c5
gopalgoel19 <mailgopalhere@gmail.com>
2017-03-07 13:06:05 +0530

    Add comment functionality

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.utils import timezone
 
-
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
@@ -17,3 +16,20 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post',related_name='comments')
+    author = models.CharField(max_length=50)
+    text = models.CharField(max_length=140)
+    publish_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=true)
+
+    def __str__(self):
+        return self.text
