0a779d413a4bd92bbf0f37213a44a00c37d63efa
gopalgoel <mailgopalhere@gmail.com>
2017-11-15 10:59:08 +0530

    Fix bug

--- blog/forms.py
+++ blog/forms.py
@@ -3,7 +3,7 @@ from django import forms
 from .models import Post, Comment
 
 class PostForm(forms.ModelForm):
-    tag = forms.CharField()
+    tag = forms.CharField(required=False)
     class Meta:
         model = Post
         fields = ('title', 'text',)
